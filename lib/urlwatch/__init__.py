a1d3d37ec443fa81bc9ba1a237f7161ab216e993
r68bux <r68bux@insomnia247.nl>
2017-11-15 02:34:28 +0100

    init

---
+++ lib/urlwatch/__init__.py
@@ -0,0 +1,16 @@
+"""A tool for monitoring webpages for updates
+
+urlwatch is intended to help you watch changes in webpages and get notified
+(via email, in your terminal or with a custom-written reporter class) of any
+changes. The change notification will include the URL that has changed and
+a unified diff of what has changed.
+"""
+
+pkgname = 'betaupdate_bot'
+
+__copyright__ = 'Copyright 2017 hell'
+__author__ = 'shit@bathroom.com>'
+__license__ = 'proprietary'
+__url__ = 'http://shit.com'
+__version__ = '1.0'
+__user_agent__ = '%s/%s (+http://faggot.com)' % (pkgname, __version__)
